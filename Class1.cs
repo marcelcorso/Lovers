91c36b3add2aa924ff803fe38bfaf3b5484a24e6
marcel corso <marcel@messagebird.com>
2022-06-28 09:46:51 +0200

    hello lovers

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace Lovers;
+
+public class Person
+{
+    // Constructor that takes no arguments:
+    public Person()
+    {
+        Name = "unknown";
+    }
+
+    // Constructor that takes one argument:
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    // Auto-implemented readonly property:
+    public string Name { get; }
+
+    // Method that overrides the base class (System.Object) implementation.
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+public class Class1
+{
+    static void Main()
+    {
+        // Call the constructor that has one parameter.
+        var person2 = new Person("Sarah Jones");
+        Console.WriteLine(person2.Name);
+        // Get the string representation of the person2 instance.
+        Console.WriteLine(person2);
+
+
+	string accessKey = Environment.GetEnvironmentVariable("MESSAGEBIRD_ACCESS_KEY");
+	MessageBird.Client client = MessageBird.Client.CreateDefault(accessKey);
+	Console.WriteLine(client.Balance());
+
+	Console.WriteLine(SillyLib.Silly.Add(4, 6));
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+this is a simple executable that uses a library in c#
+
+# Do it again
+
+```
+
+
+dotnet add package MessageBird
+```
+
+
+# Run it 
+
+```
+export MESSAGEBIRD_ACCESS_KEY=SECRET
+dotnet run 
+```
